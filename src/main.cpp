8814321cb02aeb7b97f5b5b04a57603dad1a393e
painnick <painnick@gmail.com>
2024-08-24 00:37:52 +0900

    REFACTORING. Tower Controller & Add Base codes

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #define MAIN_TAG "Main"
 
+// Tower PIN
 #define PIN_HALL_SENSOR_MAX 21
 #define PIN_HALL_SENSOR_MIN 22
 
@@ -11,106 +12,51 @@
 #define PIN_MOTOR1 25
 #define PIN_MOTOR2 26
 
+// Base PIN & Channel
+#define PIN_SERVO1_TABLE 14
+#define PIN_SERVO1_ARM 17
+#define PIN_EYE 4
+#define PIN_GUN 18
+
 #define CH_MOTOR1 2
 #define CH_MOTOR2 3
 
-boolean motorStop = false;
-boolean isMotorUp = false;
+// Servo
+#define DISABLE_COMPLEX_FUNCTIONS
+#define MAX_EASING_SERVOS 6
+#define DISABLE_MICROS_AS_DEGREE_PARAMETER
 
-boolean nearMax = false;
-boolean nearMin = false;
+#include <ServoEasing.hpp>
 
-void motorMoveUp(boolean noLimit = false) {
-    if (noLimit) {
-        ESP_LOGI(MAIN_TAG, "Motor up(NO LIMIT)...");
-        motorStop = false;
-        isMotorUp = true;
-        ledcWrite(CH_MOTOR1, 255);
-        ledcWrite(CH_MOTOR2, 0);
-    } else {
-        if (nearMax) {
-            ESP_LOGW(MAIN_TAG, "Skip Motor up!");
-        } else {
-            ESP_LOGI(MAIN_TAG, "Motor up...");
-            motorStop = false;
-            isMotorUp = true;
-            ledcWrite(CH_MOTOR1, 255);
-            ledcWrite(CH_MOTOR2, 0);
-        }
-    }
-}
+// Controllers
+#include "controllers/BaseController.h"
+#include "controllers/TowerController.h"
 
-void motorMoveDown(boolean noLimit = false) {
-    if (noLimit) {
-        ESP_LOGI(MAIN_TAG, "Motor down(NO LIMIT)...");
-        motorStop = false;
-        isMotorUp = false;
-        ledcWrite(CH_MOTOR1, 0);
-        ledcWrite(CH_MOTOR2, 255);
-    } else {
-        if (nearMin) {
-            ESP_LOGW(MAIN_TAG, "Skip Motor down!");
-        } else {
-            ESP_LOGI(MAIN_TAG, "Motor down...");
-            motorStop = false;
-            isMotorUp = false;
-            ledcWrite(CH_MOTOR1, 0);
-            ledcWrite(CH_MOTOR2, 255);
-        }
-    }
-}
-
-void motorMoveStop() {
-    ESP_LOGI(MAIN_TAG, "Motor stop");
-    motorStop = true;
-    ledcWrite(CH_MOTOR1, 0);
-    ledcWrite(CH_MOTOR2, 0);
-}
+BaseController Base(PIN_SERVO1_TABLE, PIN_SERVO1_ARM, PIN_EYE, PIN_GUN);
+TowerController Tower(PIN_MOTOR1, PIN_MOTOR2, CH_MOTOR1, CH_MOTOR2);
 
 void IRAM_ATTR WhenMin() {
-    ESP_LOGW(MAIN_TAG, "Min!");
-    motorMoveStop();
-    nearMin = true;
+    Tower.onMin();
 }
 
 void IRAM_ATTR WhenMax() {
-    ESP_LOGW(MAIN_TAG, "Max!");
-    motorMoveStop();
-    nearMax = true;
+    Tower.onMax();
 }
 
 void setup() {
     ESP_LOGI(MAIN_TAG, "Setup!");
+
+    Tower.setup();
     attachInterrupt(PIN_HALL_SENSOR_MAX, WhenMax, FALLING);
     attachInterrupt(PIN_HALL_SENSOR_MIN, WhenMin, FALLING);
 
-    ledcSetup(CH_MOTOR1, 1000, 8);
-    ledcAttachPin(PIN_MOTOR1, CH_MOTOR1);
-    ledcSetup(CH_MOTOR2, 1000, 8);
-    ledcAttachPin(PIN_MOTOR2, CH_MOTOR2);
+    Base.setup();
+    delay(1000 * 3);
 }
 
 void loop() {
     auto isUp = touchRead(PIN_TOUCH_UP) < 32;
     auto isDown = touchRead(PIN_TOUCH_DOWN) < 32;
-    if (isUp && isDown) {
-        if (isMotorUp) {
-            motorMoveUp(true);
-        } else {
-            // Should be stop!
-            // motorMoveDown(true);
-        }
-    } else {
-        if (isUp) {
-            motorMoveUp();
-            nearMin = false;
-        } else if (isDown) {
-            motorMoveDown();
-            nearMax = false;
-        } else {
-            if (!motorStop)
-                motorMoveStop();
-        }
-    }
+    Tower.loop(isUp, isDown);
     delay(100);
 }
