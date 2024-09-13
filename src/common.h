87dbb3f0d5a218cdf8775a0245679e80ae8af466
painnick <painnick@gmail.com>
2024-09-14 02:13:31 +0900

    FIX. mp3 PIN

--- src/common.h
+++ src/common.h
@@ -4,8 +4,8 @@
 // Recommended pins include 2,4,12-19,21-23,25-27,32-33
 // for the ESP32-S2 the GPIO pins are 1-21,26,33-42
 
-#define PIN_MP3_TX 6 // OK
-#define PIN_MP3_RX 5 // OK
+#define PIN_MP3_TX 5 // OK
+#define PIN_MP3_RX 6 // OK
 
 // Tower PIN
 #define PIN_HALL_SENSOR_MAX 3 // OK
